f91b65ee83111896046f8eb949da244dcc2d5e09
KRISH_THEEBA <59197696+krishtheeba@users.noreply.github.com>
2026-05-31 10:22:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import timezone, timedelta
 from streamlit_autorefresh import st_autorefresh
 import time as tm
 import requests
+import json
 from google import genai
 
 API_KEY = st.secrets["my_api_key"]
